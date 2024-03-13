a05852ff20bb0e530cd8f2af3f96d0b7642818ad
cristofer.crego <cregocrisoferlf@gmail.com>
2024-03-13 17:27:25 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
 
 //imports Components
 import App from "./App.jsx";
 import store from "./Redux/Store.js";
 
+// axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://pi-countriesback.up.railway.app";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
